d4a537e96b6fb45a49f9be940c326cf957cc9475
zy <1581273984@qq.com>
2019-04-11 18:03:50 +0800

    完成顶部用户信息菜单与左边菜单栏的DOM结构搭建

--- src/App.js
+++ src/App.js
@@ -1,53 +1,61 @@
 import React, { Component } from 'react';
 import { Layout } from "antd";
+import { HashRouter as Router, Route } from "react-router-dom";
 import NetMenu from "./components/Menu/";
+import NetHeader from "./components/NetHeader/";
 import './App.less';
+
+// 路由懒加载--划重点：https://zh-hans.reactjs.org/docs/code-splitting.html
+
 const { Header, Sider, Content, Footer } = Layout;
 
 class App extends Component {
-  state = {
-    data: null
-  }
+    state = {
+        data: null
+    }
 
-  componentWillMount() {
-    // window.axios.get('/login/cellphone', {
-    //   params: {
-    //     phone: '17620410119',
-    //     password: 'zly119505'
-    //   }
-    // }).then((response) => {
+    componentWillMount() {
+        // window.axios.get('/login/cellphone', {
+        //   params: {
+        //     phone: '17620410119',
+        //     password: 'zly119505'
+        //   }
+        // }).then((response) => {
 
-    // })
-  }
+        // })
+    }
 
-  componentDidMount() {
+    componentDidMount() {
 
-  }
+    }
 
-  componentWillUnmount() {
+    componentWillUnmount() {
 
-  }
+    }
 
-  render() {
-    return (
-      <Layout>
-        <Header style={{ padding: 0 }}>
-          <div style={{ background: '#c62f2f', width: '100vw', height: 64 }}>
+    render() {
+        return (
+            <Layout>
+                <Header style={{ padding: 0 }}>
+                    <NetHeader />
+                </Header>
+                <Layout>
+                    <Sider className='menuWrapper'>
+                        <NetMenu />
+                        <div className='palyingSong'></div>
+                    </Sider>
+                    <Content>
+                        <Router>
 
-          </div>
-        </Header>
-        <Layout>
-          <Sider className='menuWrapper'>
-              <NetMenu />>
-          </Sider>
-          <Content></Content>
-        </Layout>
-        <Footer style={{ borderTop: '1px solid #e8e8e8' }}>
+                        </Router>
+                    </Content>
+                </Layout>
+                <Footer style={{ borderTop: '1px solid #e8e8e8' }}>
 
-        </Footer>
-      </Layout>
-    );
-  }
+                </Footer>
+            </Layout>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/NetHeader/index.js
@@ -0,0 +1,124 @@
+import React, { Component, Fragment } from 'react';
+import { Row, Col, Input, Avatar, Icon, Popover, Button, Divider } from "antd";
+import './index.less';
+const Search = Input.Search;
+
+class Header extends Component {
+
+    state = {
+        data: null
+    }
+
+    componentWillMount() {
+        
+    }
+
+    componentDidMount() {
+
+    }
+
+    componentWillUnmount() {
+
+    }
+
+
+    getPopoverContent() {
+        const elem = (
+            <Fragment>
+                <div style={{ width: 270, borderBottom: '1px solid #e8e8e8' }}>
+                    <div style={{ padding: '12px 16px' }}>
+                        <div style={{ display: 'flex', justifyContent: 'space-between', justifyItems: 'center' }}>
+                            <span>
+                                <Avatar icon="user" size={40} />
+                                <span style={{ margin: '0 10px' }}>Mr-Z提线木偶</span>
+                            </span>
+                            <Button className='punchIn' icon="smile">签到</Button>
+                        </div>
+                        <div className='userInfo'>
+                            <div>
+                                <div className='number'>1</div>
+                                <div>动态</div>
+                            </div>
+                            <Divider type='vertical' className='divider' />
+                            <div>
+                                <div className='number'>8</div>
+                                <div>关注</div>
+                            </div>
+                            <Divider type='vertical' className='divider' />
+                            <div>
+                                <div className='number'>1</div>
+                                <div>粉丝</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div style={{borderBottom: '1px solid #e8e8e8'}}>
+                    <div className='userinfoMenu'>
+                        <span><Icon type="customer-service" />会员中心</span>
+                        <Icon type="right" className='righIcon' />
+                    </div>
+                    <div className='userinfoMenu'>
+                        <span><Icon type="crown" />等级</span>
+                        <Icon type="right" className='righIcon' />
+                    </div>
+                    <div className='userinfoMenu'>
+                        <span><Icon type="shopping-cart" />商城</span>
+                        <Icon type="right" className='righIcon' />
+                    </div>
+                </div>
+                <div style={{borderBottom: '1px solid #e8e8e8'}}>
+                    <div className='userinfoMenu'>
+                        <span><Icon type="setting" />个人信息设置</span>
+                        <Icon type="right" className='righIcon' />
+                    </div>
+                    <div className='userinfoMenu'>
+                        <span><Icon type="mobile" />绑定社交账号</span>
+                        <Icon type="right" className='righIcon' />
+                    </div>
+                </div>
+                <div>
+                    <div className='userinfoMenu'>
+                        <span><Icon type="poweroff" />退出登录</span>
+                    </div>
+                </div>
+            </Fragment>
+        )
+        return elem;
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <Row style={{ background: '#c62f2f', width: '100vw', height: 64 }}>
+                        <Col span={3} style={{fontFamily: 'Arial, Helvetica, sans-serif', fontWeight: 'bold', fontSize: 20, color: 'white', textAlign: 'center'}}>
+                            网易云音乐
+                        </Col>
+                        <Col span={21} className='headerRightCol'>
+                            <div>
+                                <Search
+                                    placeholder="搜索音乐，视频，歌词，电台"
+                                    onSearch={value => console.log(value)}
+                                    className='search'
+                                />
+                            </div>
+                            <div className='rightdiv'>
+                                <Avatar icon="user" className='avatar' />
+                                <Popover
+                                    overlayClassName='userinfoPop'
+                                    trigger='click'
+                                    content={this.getPopoverContent()}
+                                >
+                                    <span className='nickName'>Mr-Z提线木偶 <Icon type="caret-down" /></span>
+                                </Popover>
+                                <span><Icon type="skin" /></span>
+                                <span><Icon type="mail" /></span>
+                                <span><Icon type="setting" /></span>
+                            </div>
+                        </Col>
+                    </Row>
+            </Fragment>
+        );
+    }
+}
+
+export default Header;
